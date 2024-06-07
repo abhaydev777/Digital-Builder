7b6fbf3001c9252456423b8b13a1f3cb658808f8
abhaydev777 <abhay701734@gmail.com>
2024-06-07 12:00:38 +0530

    adding some styles
    adding some styles to the fbr routes

--- app/fbr/aboutus/page.jsx
+++ app/fbr/aboutus/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const aboutus = () => {
   return (
-    <main className=' w-full min-h-[100vh]'>
+    <main className=' w-full min-h-[100vh] flex justify-center items-center'>
         <h1>About Us</h1>
       </main>
   )
--- app/fbr/portfolio/page.jsx
+++ app/fbr/portfolio/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const portfolio = () => {
   return (
-    <main className=' w-full min-h-[100vh]'>
+    <main className=' w-full min-h-[100vh] flex justify-center items-center'>
         <h1>Portfolio</h1>
       </main>
   )
